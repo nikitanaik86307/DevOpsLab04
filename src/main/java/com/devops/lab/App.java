8ce3ae1a059e992124e22cb6c7238578c946031d
nikitanaik86307 <2021wa86307@wilp.bits-pilani.ac.in>
2025-07-14 20:59:40 +0530

    Update App.java

--- src/main/java/com/devops/lab/App.java
+++ src/main/java/com/devops/lab/App.java
@@ -5,6 +5,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello from continues integration Triggers!" );
-	System.out.println("Devops Lab 4 is completed");
+	System.out.println("Devops Lab 4 is completes if the first line of output shows Started by SCM");
+	    System.out.println( 2 + 2 );
+	    
     }
 }

0e966d85925e4e534e76e6aa256b3012932c3f1c
cloud@2021wa86307 <2021wa86307@wilp.bits-pilani.ac.in>
2025-07-14 20:42:54 +0530

    Updated App.java locally

--- src/main/java/com/devops/lab/App.java
+++ src/main/java/com/devops/lab/App.java
@@ -1,13 +1,10 @@
 package com.devops.lab;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello from continues integration Triggers!" );
+	System.out.println("Devops Lab 4 is completed")
     }
 }

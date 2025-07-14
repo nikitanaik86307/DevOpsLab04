cacf43d60f4b7a2cb2c3cffd565c1db77fa8cc27
nikitanaik86307 <2021wa86307@wilp.bits-pilani.ac.in>
2025-07-14 20:56:01 +0530

    Update App.java

--- src/main/java/com/devops/lab/App.java
+++ src/main/java/com/devops/lab/App.java
@@ -5,6 +5,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello from continues integration Triggers!" );
-	System.out.println("Devops Lab 4 is completed")
+	System.out.println("Devops Lab 4 is completed");
     }
 }
